759cbeac5e1264ae6ed2c38d65ae3e7c287e1b59
Alexander Babin <alexander_babin@mckinsey.com>
2023-12-06 12:59:38 +0700

    prepare template for day7

---
+++ src/main/scala/AdventOfCode/Day7/Day7second.scala
@@ -0,0 +1,16 @@
+package AdventOfCode.Day7
+
+object Day7second {
+  private val day: String = "day7"
+  private val postfix: String = ".input"
+
+  def parseLine(xs: String): List[Long] =
+    xs.split(":").last.trim.split(" ").filter(_.nonEmpty).map(x => x.toLong).toList
+
+  def execute(postfix: String) =
+    val lines = AdventOfCode.readLine(day, postfix)
+    println(lines)
+
+  @main def mainSecond: Unit =
+    println(execute(postfix))
+}
